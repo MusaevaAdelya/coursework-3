bca889f89610c81c44cc6e0b481e1a77e1d284b8
Adelya <msvadelya@gmail.com>
2022-11-21 01:58:50 +0600

    added courses from database to main page

--- src/main/java/edu/inai/coursework3/repositories/CourseRepository.java
+++ src/main/java/edu/inai/coursework3/repositories/CourseRepository.java
@@ -1,10 +1,17 @@
 package edu.inai.coursework3.repositories;
 
 import edu.inai.coursework3.entities.Course;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CourseRepository extends JpaRepository<Course,Long> {
+
+    @Query("select c from Course c where c.enabled = :enabled")
+    Page<Course> getCourses(@Param("enabled") boolean enabled, Pageable pageable);
 
 }
